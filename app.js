8bbb3351793d7cf4e86ec4f0616d16cebbbbc4ec
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-16 19:44:14 -0400

    Setup server with get command.

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+require('dotenv').load();
 var express = require('express');
+var db = require('./config/db');
+var Note = require('./models/note');
 var app = express();
+
+app.use(function(request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  next();
+});
+
+app.get('/', function(request, response) {
+  Note
+    .find()
+    .then(function(notes) {
+      response.json(notes);
+    });
+});
 
 app.listen(3030, function() {
   console.log('Listening on http://localhost:3030...');
