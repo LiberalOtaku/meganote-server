794318344227ec2b00ff92025a29b857e6497161
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-17 09:44:31 -0400

    POST command and README added.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').load();
 var express = require('express');
 var db = require('./config/db');
 var Note = require('./models/note');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.use(function(request, response, next) {
@@ -15,6 +17,23 @@ app.get('/', function(request, response) {
     .find()
     .then(function(notes) {
       response.json(notes);
+    });
+});
+
+app.use(bodyParser.json());
+
+app.post('/', function(request, response) {
+  var note = new Note({
+    title: request.body.note.title,
+    body_html: request.body.note.body_html
+  });
+  note
+    .save()
+    .then(function(noteData) {
+      response.json({
+        message: "Note added successfully!",
+        note: noteData
+      });
     });
 });
 
