23ab351431bf8cb83ed8bbad6b5e1ad5a571724e
LiberalOtaku <ledgam3r1279@gmail.com>
2016-07-12 14:45:24 -0400

    Separated schema from models.

--- models/user.js
+++ models/user.js
@@ -1,44 +1,4 @@
 var db = require('../config/db');
-var bcrypt = require('bcryptjs');
+var userSchema = require('./userSchema');
 
-var userSchema = db.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password_digest: {
-    type: String,
-    required: true,
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-userSchema.pre('save', function(next) {
-  this.updated_at = Date.now();
-  next();
-});
-
-userSchema.methods.toJSON = function() {
-  var user = this.toObject();
-  delete user.password_digest;
-  delete user.__v;
-  return user;
-};
-
-userSchema.methods.authenticate = function(password, callback) {
-  bcrypt.compare(password, this.password_digest, (err, isMatch) => {
-    callback(isMatch);
-  });
-};
-
-var User = db.model('User', userSchema);
-
-module.exports = User;
+module.exports = db.model('User', userSchema);
